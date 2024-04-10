88146173b78e4f57224504f9f0240cb3f9a92e8a
Golobon <golobon@ya.ru>
2024-04-10 20:55:06 +0300

    Good

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import kotlin.jvm.internal.Intrinsics.Kotlin
 
 const val AGE1 = 5
 const val AGE2 = 20
@@ -5,10 +6,19 @@ const val AGE2 = 20
 fun main(args: Array<String>) {
     val dd : Boolean
 
-    val int1 = 4
-    var int2 = 4
+    val int1 = 4; var int2 = 4
 
-    println(int1 !in AGE1..AGE2)
+    val result = if (int2 > int2) "ddd" else "ff"
 
+    val x = when (readln()!!.toInt()) {
+        10 -> {
+            "10".chars(); println("dd");
+        }
+        20 -> "20"
+        30 -> "30"
+        else -> "Her"
+    }
+    println(x::toString)
+    kotlin.Unit
 }
 
